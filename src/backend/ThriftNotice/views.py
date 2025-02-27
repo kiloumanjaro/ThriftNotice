95552cd9080a6f45c8a82a277e80caefe7910d6b
JohSANCoding <129604435+JohSANCoding@users.noreply.github.com>
2025-02-27 19:37:40 +0800

    Update views.py
    - Coordinates Now Include Shop ID

--- src/backend/ThriftNotice/views.py
+++ src/backend/ThriftNotice/views.py
@@ -19,7 +19,7 @@ class ThriftStoreViewSet(viewsets.ModelViewSet):
         Custom action to retrieve only latitude and longitude for map display.
         URL will be: /api/thriftstores/map-locations/ (because detail=False)
         """
-        queryset = self.queryset.values('latitude', 'longitude') # Efficiently select lat/long
+        queryset = self.queryset.values('latitude', 'longitude', 'shopid') # Efficiently select lat/long
         return Response(list(queryset), status=status.HTTP_200_OK)
 
 class UsersViewSet(viewsets.ModelViewSet):
